c9d2d81f965036ce3d0415569eaba4605e570157
CheeJong <cheejong09@gmail.com>
2021-11-20 14:52:02 +0800

    Applied E-commerce Template 2

--- js/main.js
+++ js/main.js
@@ -1,173 +1,217 @@
-/*  ---------------------------------------------------
-    Template Name: Violet 
-    Description: Violet ecommerce Html Template
-    Author: Colorlib
-    Author URI: https://colorlib.com/
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
+ AOS.init({
+ 	duration: 800,
+ 	easing: 'slide',
+ 	once: true
+ });
 
-'use strict';
+jQuery(document).ready(function($) {
 
-(function ($) {
+	"use strict";
 
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-
-        /*------------------
-		    Product filter
-	    --------------------*/
-        if ($('#product-list').length > 0) {
-            var containerEl = document.querySelector('#product-list');
-            var mixer = mixitup(containerEl);
+	var slider = function() {
+		$('.nonloop-block-3').owlCarousel({
+	    center: false,
+	    items: 1,
+	    loop: false,
+			stagePadding: 15,
+	    margin: 20,
+	    nav: true,
+			navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+	    responsive:{
+        600:{
+        	margin: 20,
+          items: 2
+        },
+        1000:{
+        	margin: 20,
+          items: 3
+        },
+        1200:{
+        	margin: 20,
+          items: 3
         }
-    });
-
-    /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
-    });
-
-    /*------------------
-		Navigation
-	--------------------*/
-    $(".mobile-menu").slicknav({
-        appendTo: '.header-section',
-        allowParentLinks: true,
-        closedSymbol: '<i class="fa fa-angle-right"></i>',
-		openedSymbol: '<i class="fa fa-angle-down"></i>'
-    });
-
-    /*------------------
-		Search model
-	--------------------*/
-	$('.search-trigger').on('click', function() {
-		$('.search-model').fadeIn(400);
-	});
-
-	$('.search-close-switch').on('click', function() {
-		$('.search-model').fadeOut(400,function(){
-			$('#search-input').val('');
+	    }
 		});
-	});
+	};
+	slider();
 
-    /*------------------
-        Carousel Slider
-    --------------------*/
-     $(".hero-items").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        items: 1,
-        dots: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        smartSpeed: 1200,
-        autoplayHoverPause: true,
-        mouseDrag: false,
-        autoplay: false,
+
+	var siteMenuClone = function() {
+
+		$('<div class="site-mobile-menu"></div>').prependTo('.site-wrap');
+
+		$('<div class="site-mobile-menu-header"></div>').prependTo('.site-mobile-menu');
+		$('<div class="site-mobile-menu-close "></div>').prependTo('.site-mobile-menu-header');
+		$('<div class="site-mobile-menu-logo"></div>').prependTo('.site-mobile-menu-header');
+
+		$('<div class="site-mobile-menu-body"></div>').appendTo('.site-mobile-menu');
+
+		
+
+		$('.js-logo-clone').clone().appendTo('.site-mobile-menu-logo');
+
+		$('<span class="ion-ios-close js-menu-toggle"></div>').prependTo('.site-mobile-menu-close');
+		
+
+		$('.js-clone-nav').each(function() {
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
+
+
+		setTimeout(function() {
+			
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+        
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
+      });
+
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();  
+      
     });
 
-    /*------------------
-        Carousel Slider
-    --------------------*/
-    $(".logo-items").owlCarousel({
-        loop: true,
-		nav: false,
-		dots: false,
-		margin : 40,
-		autoplay: true,
-        responsive: {
-            0: {
-                items: 2
-            },
-            480: {
-                items: 2
-            },
-            768: {
-                items: 3
-            },
-            992: {
-                items: 5
-            }
-        }
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
+
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		}) 
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	}; 
+	siteMenuClone();
+
+
+	var sitePlusMinus = function() {
+		$('.js-btn-minus').on('click', function(e){
+			e.preventDefault();
+			if ( $(this).closest('.input-group').find('.form-control').val() != 0  ) {
+				$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1);
+			} else {
+				$(this).closest('.input-group').find('.form-control').val(parseInt(0));
+			}
+		});
+		$('.js-btn-plus').on('click', function(e){
+			e.preventDefault();
+			$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1);
+		});
+	};
+	sitePlusMinus();
+
+
+	var siteSliderRange = function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
     });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+	};
+	siteSliderRange();
 
 
-    /*------------------
-        Carousel Slider
-    --------------------*/
-    $(".product-slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: false,
-        items: 1,
-        dots: true,
-        autoplay: true,
-    });
-    
+	var siteMagnificPopup = function() {
+		$('.image-popup').magnificPopup({
+	    type: 'image',
+	    closeOnContentClick: true,
+	    closeBtnInside: false,
+	    fixedContentPos: true,
+	    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+	     gallery: {
+	      enabled: true,
+	      navigateByImgClick: true,
+	      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	    },
+	    image: {
+	      verticalFit: true
+	    },
+	    zoom: {
+	      enabled: true,
+	      duration: 300 // don't foget to change the duration also in CSS
+	    }
+	  });
 
-    /*------------------
-        Magnific Popup
-    --------------------*/
-    $('.pop-up').magnificPopup({
-        type: 'image'
-    });
+	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+	    disableOn: 700,
+	    type: 'iframe',
+	    mainClass: 'mfp-fade',
+	    removalDelay: 160,
+	    preloader: false,
 
-    /*-------------------
-		Sort Select
-	--------------------- */
-    $('.sort').niceSelect();
+	    fixedContentPos: false
+	  });
+	};
+	siteMagnificPopup();
 
-    /*-------------------
-		Cart Select
-	--------------------- */
-    $('.cart-select').niceSelect();
+	var searchShow = function() {
+		// alert();
+		var searchWrap = $('.search-wrap');
+		$('.js-search-open').on('click', function(e) {
+			e.preventDefault();
+			searchWrap.addClass('active');
+			setTimeout(function() {
+				searchWrap.find('.form-control').focus();
+			}, 300);
+		});
+		$('.js-search-close').on('click', function(e) {
+			e.preventDefault();
+			searchWrap.removeClass('active');
+		})
+	};
+	searchShow();
 
-    /*-------------------
-		Quantity change
-	--------------------- */
-    var proQty = $('.pro-qty');
-    proQty.prepend('<span class="dec qtybtn">-</span>');
-    proQty.append('<span class="inc qtybtn">+</span>');
-    proQty.on('click', '.qtybtn', function () {
-        var $button = $(this);
-        var oldValue = $button.parent().find('input').val();
-        if ($button.hasClass('inc')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            // Don't allow decrementing below zero
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        $button.parent().find('input').val(newVal);
-    });
-
-    /*-------------------
-		Radio Btn
-	--------------------- */
-    $(".shipping-info .cs-item label").on('click', function () {
-        $(".shipping-info .cs-item label").removeClass('active');
-        $(this).addClass('active');
-    });
-
-    $(".checkout-form .diff-addr label").on('click', function () {
-        $(this).toggleClass('active');
-    });
-
-    $(".payment-method ul li label").on('click', function () {
-        $(this).toggleClass('active');
-    });
-
-})(jQuery);
+});
